f5b1d525d4f4a6b55e854f079cb32b4dff5ef99c
mccray austin <tubach75@hotmail.com>
2020-10-17 12:36:04 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,7 +1,7 @@
 class Post
   attr_accessor :title, :author
 
-  def initialize(title, author)
+  def initialize(title = "checks", author = "checkov")
     @title = title
     @author = author
   end
